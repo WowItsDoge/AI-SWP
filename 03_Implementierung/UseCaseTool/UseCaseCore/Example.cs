f660bab0ed2247e3bb300b84b29c86de812546af
JanMende <mende.jan@web.de>
2018-04-23 12:23:01 +0200

    Projekt erstellt

---
+++ 03_Implementierung/UseCaseTool/UseCaseCore/Example.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCaseCore
+{
+    public class Example
+    {
+        public static string GetMessage()
+        {
+            return "Hello world!";
+        }
+    }
+}
---
+++ 03_Implementierung/UseCaseTool/UseCaseTest/ExampleTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UseCaseCore;
+using NUnit.Framework;
+
+namespace UseCaseTest
+{
+    [TestFixture]
+    public class ExampleTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            string message = Example.GetMessage();
+
+            Assert.IsNotNull(message);
+        }
+    }
+}
