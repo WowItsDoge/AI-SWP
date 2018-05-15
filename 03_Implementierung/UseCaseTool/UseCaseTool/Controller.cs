a9079ef18eec2f346c62bb9337b93ec2a51bfb4d
NicoHanke <38317553+NicoHanke@users.noreply.github.com>
2018-05-15 17:23:13 +0200

    GUI und Controller hinzugefügt
    GUI und Controller hinzugefügt

---
+++ 03_Implementierung/UseCaseTool/UseCaseTool/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCaseTool
+{
+    class Controller
+    {
+        //XMLStructureParser xmlParser = new XMLStructureParser;
+        public void currentXmlFilePath(string filePath)
+        {
+          //  xmlParser.starteParsing(filePath);
+        }
+
+        public void matrixFilePath(string filePath)
+        {
+            //  ...
+        }
+
+        public void reportFilePath(string filePath)
+        {
+            //  ...
+        }
+
+
+    }
+}
