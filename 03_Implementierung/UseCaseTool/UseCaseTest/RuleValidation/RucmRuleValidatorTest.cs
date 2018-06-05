0b2b997635de8ad81a628f6a41cd6740f2dd03d6
Dennis Ruß <38317488+DennisRuss@users.noreply.github.com>
2018-06-05 13:08:32 +0200

    StyleCop angepasst
    StyleCop aus Testprojekt entfernt und Regel 1650 mit Anpassungen wieder eingeführt

--- 03_Implementierung/UseCaseTool/UseCaseTest/RuleValidation/RucmRuleValidatorTest.cs
+++ 03_Implementierung/UseCaseTool/UseCaseTest/RuleValidation/RucmRuleValidatorTest.cs
@@ -5,8 +5,11 @@
 namespace UseCaseTest.RuleValidation
 {
     using NUnit.Framework;
+    using RucmRules;
+    using System.Collections.Generic;
     using UseCaseCore.RuleValidation;
     using UseCaseCore.RuleValidation.RucmRules;
+    using UseCaseCore.XmlParser;
 
     /// <summary>
     /// Test class for RUCMRuleValidator
@@ -19,8 +22,34 @@ namespace UseCaseTest.RuleValidation
         /// </summary>
         [Test]
         public void ValidateTest()
-        {            
-            var rucmRuleValidator = new RucmRuleValidator(RuleRepository.Rules);
+        {
+            var dummyBasicFlow = new BasicFlow();
+
+            var dummy1 = new DummyRule(0);
+            var dummy2 = new DummyRule(1);
+            var dummy3 = new DummyRule(2);
+
+            // Test validation with 3 rules and 3 errors
+            var ruleList = new List<IRule> { dummy1, dummy2, dummy3 };
+            var rucmRuleValidator = new RucmRuleValidator(ruleList);
+            var result = rucmRuleValidator.Validate(dummyBasicFlow);
+
+            Assert.IsFalse(result);
+            Assert.IsTrue(rucmRuleValidator.GetErrorReport().GetErrorList.Count == 3);
+
+            // Test validation with 2 rules and 2 errors
+            ruleList = new List<IRule> { dummy1, dummy3 };
+            rucmRuleValidator = new RucmRuleValidator(ruleList);
+            result = rucmRuleValidator.Validate(dummyBasicFlow);
+            Assert.IsFalse(result);
+            Assert.IsTrue(rucmRuleValidator.GetErrorReport().GetErrorList.Count == 2);
+
+            // Test validation with 1 rules and 0 errors
+            ruleList = new List<IRule> { dummy1 };
+            rucmRuleValidator = new RucmRuleValidator(ruleList);
+            result = rucmRuleValidator.Validate(dummyBasicFlow);
+            Assert.IsTrue(result);
+            Assert.IsTrue(rucmRuleValidator.GetErrorReport().GetErrorList.Count == 0);
         }
     }
 }
