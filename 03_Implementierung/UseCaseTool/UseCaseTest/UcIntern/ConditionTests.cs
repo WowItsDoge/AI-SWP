4c1f0370accde59122686bac2ec5e1884e077c1b
dominik-rose-oth <dominik-rose-oth@users.noreply.github.com>
2018-06-06 10:11:05 +0200

    Added tests for condition

---
+++ 03_Implementierung/UseCaseTool/UseCaseTest/UcIntern/ConditionTests.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="ConditionTests.cs" company="Team B">
+//      Team B. All rights reserved.
+// </copyright>
+
+namespace UseCaseTest.UcIntern
+{
+    using NUnit.Framework;
+    using UseCaseCore.UcIntern;
+
+    /// <summary>
+    /// A class collecting all tests for the <see cref="Condition"/> struct.
+    /// </summary>
+    [TestFixture]
+    public class ConditionTests
+    {
+        /// <summary>
+        /// Sets all fields of the struct and reads them.
+        /// </summary>
+        [Test]
+        public void SetContent()
+        {
+            // Arrange
+            string expectedConditionText = "Test";
+            bool expectedConditionState = true;
+            Condition c = new Condition(expectedConditionText, expectedConditionState);
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(expectedConditionText, c.ConditionText);
+            Assert.AreEqual(expectedConditionState, c.ConditionState);
+        }
+    }
+}
