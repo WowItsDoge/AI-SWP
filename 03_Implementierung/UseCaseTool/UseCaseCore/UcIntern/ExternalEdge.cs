a068907b7dc1a1ce70dc38741f941c010b6087de
dominik-rose-oth <dominik-rose-oth@users.noreply.github.com>
2018-06-07 15:12:20 +0200

    Prepared GraphBuilder
    Prepared GraphBuilder methods and introduced external edge with tests.

---
+++ 03_Implementierung/UseCaseTool/UseCaseCore/UcIntern/ExternalEdge.cs
@@ -0,0 +1,83 @@
+﻿// <copyright file="ExternalEdge.cs" company="Team B">
+//      Team B. All rights reserved.
+// </copyright>
+
+namespace UseCaseCore.UcIntern
+{
+    /// <summary>
+    /// An instance of this class represents an edge between two nodes.
+    /// <para/>
+    /// This class is used by the <see cref="GraphBuilder"/>  to specify edges between two steps.
+    /// Because the <see cref="GraphBuilder"/> creates the edges by analyzing small blocks of nodes it can happen that an edge must be created that points to a
+    /// node that is not in the block that is analyzed. To specify such an edge this class is used. It holds the step number for the current block of nodes and the
+    /// reference step to the step where it points to. Remember to increase the step number if the analyzed block was contained in a larger block of nodes.
+    /// <para/>
+    /// The edge always points from the source to the target.
+    /// </summary>
+    public struct ExternalEdge
+    {
+        /// <summary>
+        /// The number of the step in the current block.
+        /// </summary>
+        public readonly int SourceStepNumber;
+
+        /// <summary>
+        /// The target of this edge.
+        /// </summary>
+        public readonly ReferenceStep TargetStep;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExternalEdge"/> struct.
+        /// </summary>
+        /// <param name="sourceStepNumber">The number of the source step</param>
+        /// <param name="targetStep">The target step.</param>
+        public ExternalEdge(int sourceStepNumber, ReferenceStep targetStep)
+        {
+            this.SourceStepNumber = sourceStepNumber;
+            this.TargetStep = targetStep;
+        }
+
+        /// <summary>
+        /// Tests if <paramref name="x"/> is equal to <paramref name="y"/>.
+        /// </summary>
+        /// <param name="x">The first object to be compared.</param>
+        /// <param name="y">The second object to be compared.</param>
+        /// <returns>If <paramref name="x"/> is equal to <paramref name="y"/>.</returns>
+        public static bool operator ==(ExternalEdge x, ExternalEdge y)
+        {
+            return x.SourceStepNumber == y.SourceStepNumber
+                && x.TargetStep == y.TargetStep;
+        }
+
+        /// <summary>
+        /// Tests if <paramref name="x"/> is not equal to <paramref name="y"/>.
+        /// </summary>
+        /// <param name="x">The first object to be compared.</param>
+        /// <param name="y">The second object to be compared.</param>
+        /// <returns>If <paramref name="x"/> is not equal to <paramref name="y"/>.</returns>
+        public static bool operator !=(ExternalEdge x, ExternalEdge y)
+        {
+            return !(x == y);
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns>true if the specified object is equal to the current object; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            return obj is ExternalEdge && this == (ExternalEdge)obj;
+        }
+
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
+        public override int GetHashCode()
+        {
+            return BitShifter.ShiftAndWrap(this.SourceStepNumber.GetHashCode(), 1)
+                ^ this.TargetStep.GetHashCode();
+        }
+    }
+}
--- 03_Implementierung/UseCaseTool/UseCaseCore/UcIntern/GraphBuilder.cs
+++ 03_Implementierung/UseCaseTool/UseCaseCore/UcIntern/GraphBuilder.cs
@@ -4,24 +4,46 @@
 
 namespace UseCaseCore.UcIntern
 {
+    using System.Collections.Generic;
+
     /// <summary>
     /// This class transforms the use case description consisting of the different flow types into a graph representation.
     /// </summary>
     public class GraphBuilder
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="GraphBuilder"/> class.
+        /// Builds the graph.
         /// </summary>
-        /// <param name="useCase">The use case that holds the flows and receives the graph as nodes and matrices.</param>
-        public GraphBuilder(UseCase useCase)
+        /// <param name="basicFlow">The basic flow.</param>
+        /// <param name="specificAlternativeFlows">The specific alternative flows.</param>
+        /// <param name="globalAlternativeFlows">The global alternative flows.</param>
+        /// <param name="boundedAlternativeFlows">The bounded alternative flows.</param>
+        /// <param name="nodes">The nodes of all flows.</param>
+        /// <param name="edgeMatrix">The edge matrix for the nodes.</param>
+        /// <param name="conditionMatrix">The condition matrix for the flows.</param>
+        public void BuildGraph(
+            Flow basicFlow,
+            IReadOnlyList<Flow> specificAlternativeFlows,
+            IReadOnlyList<Flow> globalAlternativeFlows,
+            IReadOnlyList<Flow> boundedAlternativeFlows,
+            out IReadOnlyList<Node> nodes,
+            out Matrix<bool> edgeMatrix,
+            out Matrix<Condition?> conditionMatrix)
         {
+            nodes = null;
+            edgeMatrix = null;
+            conditionMatrix = null;
         }
 
         /// <summary>
-        /// Builds the graph.
+        /// Sets the edges in a block of nodes.
         /// </summary>
-        public void BuildGraph()
+        /// <param name="nodes">The nodes to wire.</param>
+        /// <param name="edgeMatrix">The matrix with the edges.</param>
+        public void SetEdgesInNodeBlock(IReadOnlyList<Node> nodes, out Matrix<bool> edgeMatrix)
         {
+            // out of external edges
+            edgeMatrix = null;
         }
     }
 }
